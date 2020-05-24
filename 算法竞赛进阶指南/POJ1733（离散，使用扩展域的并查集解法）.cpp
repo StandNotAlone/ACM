1ebc3a6454a48e1dd86e2a2e1e21e62f97be4109
StandNotAlone <wzcnew@icloud.com>
2020-05-24 16:20:54 +0800

    5.24

--- 算法竞赛进阶指南/POJ1733（离散，使用扩展域的并查集解法）.cpp
+++ 算法竞赛进阶指南/POJ1733（离散，使用扩展域的并查集解法）.cpp
@@ -41,22 +41,24 @@ ll find(ll x)
     return lower_bound(origin.begin(),origin.end(),x)-origin.begin();
 }
 
-//边带权版本并查集
+//拓展域版本并查集
 vector<ll>fa;//并查集数组
-vector<ll>dis;  //dis[i]记录i距离自己所在集合的队头的奇偶性
 
 void fainit()//并查集数组初始化
 {
     for(ll i=0;i<fa.size();i++) fa[i]=i;
 }
 
-ll get(ll x) //访问x所在的根节点(代表元素)  边带权版本
+ll get(ll x) //访问x所在的根节点(代表元素)
 {
-    if(x==fa[x]) return x;
-    ll root=get(fa[x]);     //递归计算集合代表元素，且压缩路径和更新距离权值
-    dis[x]^=dis[fa[x]];     //更新距离权值
-    return fa[x]=root; //路径压缩
+    return x==fa[x]?x:fa[x]=get(fa[x]);
 }
+
+void famerge(ll x,ll y)//合并x和y所在的集合，即让x的根节点作为y的树根的子节点
+{
+    fa[get(x)]=get(y);
+}
+
 
 int main()
 {
@@ -75,25 +77,32 @@ int main()
     }
     sort(origin.begin(),origin.end());
     origin.erase(unique(origin.begin(),origin.end()),origin.end());//去重
-    fa.resize(origin.size());
+    fa.resize(origin.size()*2);//并查集数组开两倍大小，+origin.size()后的下标代表偶数域，不+origin.size()的下标代表奇数域
     fainit();
-    dis.resize(origin.size(),0);
-    for(ll i=0;i<Node.size();i++)
+    for(ll i=0;i<m;i++)
     {
-        ll A=find(Node[i].l),B=find(Node[i].r);
-        ll a=get(A),b=get(B);
-        if(a==b)
+        ll l_odd=find(Node[i].l),l_even=l_odd+origin.size();
+        ll r_odd=find(Node[i].r),r_even=r_odd+origin.size();
+        if(Node[i].data)
         {
-            if((dis[A]^dis[B])!=Node[i].data)
+            if(get(l_odd)==get(r_odd))
             {
                 printf("%lld\n",i);
                 return 0;
             }
+            famerge(l_odd,r_even);
+            famerge(l_even,r_odd);
         }
         else
         {
-            fa[a]=b;
-            dis[a]=dis[A]^dis[B]^Node[i].data;
+            if(get(l_odd)==get(r_even))
+            //注意不能写成get(l_odd)!=get(r_odd)，因为在初始化的时候这就是不相等的，应该判断是否在同一个集合内
+            {
+                printf("%lld\n",i);
+                return 0;
+            }
+            famerge(l_odd,r_odd);
+            famerge(l_even,r_even);
         }
     }
     printf("%lld\n",m);
