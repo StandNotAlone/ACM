76b5bb10309bb04ae3ba1781c6806c9361d96cee
StandNotAlone <wzcnew@icloud.com>
2020-05-27 08:32:17 +0800

    5.27

---
+++ 王子超的codeforce/2020.5.26Codeforces Round #645 (Div. 2)C（思维，结论）.cpp
@@ -0,0 +1,73 @@
+#include<map>
+#include<set>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<cstdio>
+#include<vector>
+#include<bitset>
+#include<string>
+#include<cstdlib>
+#include<cstring>
+#include<fstream>
+#include<sstream>
+#include<iomanip>
+#include<iostream>
+#include<algorithm>
+#include<functional>
+#include<bits/stdc++.h>
+#define INF 0x7f7f7f7f //2139062143
+#define INF1 0x3f3f3f3f //1061109567
+#define INF2 2147483647
+#define llINF 9223372036854775807
+#define pi 3.141592653589793//23846264338327950254
+#define ft first
+#define sd second
+#define mp make_pair
+#define pb push_back
+#define ll long long
+#define vec vector<ll>
+#define mat vector<vector<ll>>
+#define rep(i,n) for(ll i=0;i<(ll)(n);i++)
+#define _rep(i,n) for(ll i=n-1;i>=0;i--)
+#define REP(i,n) for(ll i=1;i<=(ll)(n);i++)
+#define _REP(i,n) for(ll i=n;i>0;i--)
+#define at(x,n) for(auto &x:n)
+//cout<<fixed<<setprecision(6)<<
+//freopen(".in","r",stdin);
+//freopen(".out","w",stdout);
+//ifstream f1("/Users/wangzichao/Documents/wzc.in");
+//ofstream f2("/Users/wangzichao/Documents/wzc.out");
+//ios::sync_with_stdio(false);
+//cin.tie(0);cout.tie(0);
+using namespace std;
+#define local
+#ifdef local
+#endif
+const ll maxn=1e3+7;
+const double eps=1e-10;
+const ll mod=1e9+7;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll x1,y1,x2,y2;
+        cin>>x1>>y1>>x2>>y2;
+        x2-=x1;
+        y2-=y1;
+        cout<<x2*y2+1<<endl;
+//        ll ans=(x2-1)*x2;
+//        ans+=(y2-x2+1)*x2;
+//        cout<<ans+1<<endl; 此部分化简即为上面的公式，基本思路是对于某一块来说，最小的和的路径是先往右到顶再向下
+                            //最大的和的路径是先向下到顶再向右，这两个值之间的所有值都可以被取到，原因自行论证
+    }
+}
+
+
