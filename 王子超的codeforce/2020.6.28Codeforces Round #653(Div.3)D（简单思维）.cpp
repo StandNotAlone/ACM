ea256b1b0a06884b5b53e0da412ecc4bc35a73d0
StandNotAlone <wzcnew@icloud.com>
2020-07-03 21:03:08 +0800

    7.3

---
+++ 王子超的codeforce/2020.6.28Codeforces Round #653(Div.3)D（简单思维）.cpp
@@ -0,0 +1,106 @@
+#include<map>
+#include<set>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<cstdio>
+#include<vector>
+#include<bitset>
+#include<string>
+#include<cstdlib>
+#include<cstring>
+#include<fstream>
+#include<sstream>
+#include<iomanip>
+#include<iostream>
+#include<algorithm>
+#include<functional>
+#include<bits/stdc++.h>
+#define INF 0x7f7f7f7f //2139062143
+#define INF1 0x3f3f3f3f //1061109567
+#define INF2 2147483647
+#define llINF 9223372036854775807
+#define pi 3.141592653589793//23846264338327950254
+#define ft first
+#define sd second
+#define endl "\n"
+#define mp make_pair
+#define pb push_back
+#define ll long long
+#define int long long
+#define vec vector<ll>
+#define mat vector<vector<ll>>
+#define rep(i,n) for(ll i=0;i<(ll)(n);i++)
+#define _rep(i,n) for(ll i=n-1;i>=0;i--)
+#define REP(i,n) for(ll i=1;i<=(ll)(n);i++)
+#define _REP(i,n) for(ll i=n;i>0;i--)
+#define at(x,n) for(auto &x:n)
+//cout<<fixed<<setprecision(6)<<
+//freopen(".in","r",stdin);
+//freopen(".out","w",stdout);
+//ifstream f1("/Users/wangzichao/Documents/wzc.in");
+//ofstream f2("/Users/wangzichao/Documents/wzc.out");
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int read()      //整数读入挂
+{
+    int x=0,f=1;
+    char c=getchar();
+    while(c<'0'||c>'9')
+    {
+        if(c=='-') f=-1;
+        c=getchar();
+    }
+    while(c>='0'&&c<='9')
+    {
+        x=x*10+c-'0';
+        c=getchar();
+    }
+    return x*f;
+}
+
+using namespace std;
+#define local
+#ifdef local
+#endif
+const ll maxn=1e3+7;
+const double eps=1e-10;
+const ll mod=1e9+7;
+
+
+
+int32_t main()
+{
+    IOS;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,k;
+        cin>>n>>k;
+        vec num(n);
+        map<ll,ll>M;
+        at(x,num)
+        {
+            cin>>x;
+            if(x%k)
+            {
+                if(M.find(k-x%k)!=M.end())
+                {
+                    M[k-x%k]++;
+                }
+                else M[k-x%k]=1;
+            }
+        }
+        ll Max=0;
+        at(x,M)
+        {
+            ll temp=x.first+(x.second-1)*k+1;
+            Max=max(temp,Max);
+        }
+        cout<<Max<<endl;
+        M.clear();
+    }
+}
+
+
---
+++ 王子超的codeforce/2020.7.1Codeforces Round #654(Div.2)D（简单矩阵构造）.cpp
@@ -0,0 +1,105 @@
+#include<map>
+#include<set>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<cstdio>
+#include<vector>
+#include<bitset>
+#include<string>
+#include<cstdlib>
+#include<cstring>
+#include<fstream>
+#include<sstream>
+#include<iomanip>
+#include<iostream>
+#include<algorithm>
+#include<functional>
+#include<bits/stdc++.h>
+#define INF 0x7f7f7f7f //2139062143
+#define INF1 0x3f3f3f3f //1061109567
+#define INF2 2147483647
+#define llINF 9223372036854775807
+#define pi 3.141592653589793//23846264338327950254
+#define ft first
+#define sd second
+#define endl "\n"
+#define mp make_pair
+#define pb push_back
+#define ll long long
+#define int long long
+#define vec vector<ll>
+#define mat vector<vector<ll>>
+#define rep(i,n) for(ll i=0;i<(ll)(n);i++)
+#define _rep(i,n) for(ll i=n-1;i>=0;i--)
+#define REP(i,n) for(ll i=1;i<=(ll)(n);i++)
+#define _REP(i,n) for(ll i=n;i>0;i--)
+#define at(x,n) for(auto &x:n)
+//cout<<fixed<<setprecision(6)<<
+//freopen(".in","r",stdin);
+//freopen(".out","w",stdout);
+//ifstream f1("/Users/wangzichao/Documents/wzc.in");
+//ofstream f2("/Users/wangzichao/Documents/wzc.out");
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int read()      //整数读入挂
+{
+    int x=0,f=1;
+    char c=getchar();
+    while(c<'0'||c>'9')
+    {
+        if(c=='-') f=-1;
+        c=getchar();
+    }
+    while(c>='0'&&c<='9')
+    {
+        x=x*10+c-'0';
+        c=getchar();
+    }
+    return x*f;
+}
+
+using namespace std;
+#define local
+#ifdef local
+#endif
+const ll maxn=3e2+7;
+const double eps=1e-10;
+const ll mod=1e9+7;
+
+ll field[maxn][maxn];
+
+
+int32_t main()
+{
+    IOS;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,k;
+        cin>>n>>k;
+        if(k%n) cout<<2<<endl;
+        else cout<<0<<endl;
+        vec num(n+1,k/n);
+        ll temp=k%n;
+        REP(i,temp) num[i]++;
+        memset(field,0,sizeof(field));
+        REP(i,n)
+        {
+            rep(j,num[i])
+            {
+                ll tar=(i+j)%n;
+                if(tar==0) tar=n;
+                field[i][tar]=1;
+            }
+        }
+        REP(i,n)
+        {
+            REP(j,n) cout<<field[i][j];
+            cout<<endl;
+        }
+    }
+}
+
+ 
